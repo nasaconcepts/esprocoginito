216cb081b84977fd22eb1be160215c2715461f13
unknown <chinasa.obichere@gmail.com>
2024-02-21 18:11:31 +0000

    adding configuration annotation

--- src/main/java/com/espro/cognito/configs/CrossOriginConfig.java
+++ src/main/java/com/espro/cognito/configs/CrossOriginConfig.java
@@ -1,10 +1,11 @@
 package com.espro.cognito.configs;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
+@Configuration
 public class CrossOriginConfig {
     @Bean
     public CorsFilter corsFilter() {
